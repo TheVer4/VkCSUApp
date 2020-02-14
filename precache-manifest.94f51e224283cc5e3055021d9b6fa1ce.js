67a9d7252b42b4d57c754496b5a4d86a8f79028f
Nikita <thever4@Linux.TheVer4.GQ>
2020-02-15 02:09:11 +0500

    Updates

---
+++ precache-manifest.94f51e224283cc5e3055021d9b6fa1ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7901a2b3ae7b12979f1ad36c1e7ca66",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29ced53596416486d4cc",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "2026eeeaba26b0ee966e",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "29ced53596416486d4cc",
+    "url": "./static/js/2.3c95f00f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.3c95f00f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2026eeeaba26b0ee966e",
+    "url": "./static/js/main.e4b7054c.chunk.js"
+  },
+  {
+    "revision": "42eff11e174c129dbede",
+    "url": "./static/js/runtime-main.2b8ccd75.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
